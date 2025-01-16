5a857f9b96366ea1602e5d39deb5c3188f171d64
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-01-16 18:46:25 +0100

    initial commit + array

---
+++ ex1/script.js
@@ -0,0 +1,27 @@
+// Snack 1
+// Creare un array di oggetti: Ogni oggetto descriverà una bici
+//  da corsa con le seguenti proprietà: nome e peso.
+//  Stampare a schermo la bici con peso minore.
+
+const biciclette = [
+    {
+        'nome' : 'bici#1',
+        'peso' : 6,
+    },
+    {
+        'nome' : 'bici#2',
+        'peso' : 3,
+    },
+    {
+        'nome' : 'bici#3',
+        'peso' : 2,
+    },
+    {
+        'nome' : 'bici#4',
+        'peso' : 7,
+    },
+    {
+        'nome' : 'bici#5',
+        'peso' : 9,
+    },
+]
